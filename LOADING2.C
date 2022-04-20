589c733525e47c16954b949ecfabe15226bfba26
jikjatin <jatinrajput04@gmail.com>
2022-04-20 20:07:24 +0530

    loading screen program in C

---
+++ LOADING2.C
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+void main()
+{
+	int i;
+
+	gotoxy(1,15);
+	for(i=0;i<80;i++)
+	{
+		printf("_",i);
+
+	}
+	gotoxy(1,17);
+	for(i=0;i<80;i++)
+	{
+		printf("_",i);
+
+	}
+	gotoxy(1,16);
+	for(i=0;i<80;i++)
+	{
+		printf("%c",176);
+
+	}
+	gotoxy(1,16);
+	for(i=0;i<80;i++)
+	{
+		textcolor(GREEN);
+		cprintf("%c",177);
+		delay(50);
+
+
+	}
+	getch();
+}
